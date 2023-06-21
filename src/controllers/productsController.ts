639cbd443d0bf14fa2ccc6878c594b40c37d3e9d
Juan Sued Oliveira de Souza <juan.sued@lumis.com.br>
2023-06-21 10:30:37 -0300

    implementada rota update product

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -2,6 +2,7 @@ import { products } from '@prisma/client';
 import { productsService } from '@/services';
 import { Request, Response } from 'express';
 import { ProductBasic } from '@/interfaces/productsInterfaces';
+import { productsRepository } from '@/repositories';
 
 export async function insertProduct(request: Request, response: Response) {
   const newProduct: products = request.body;
@@ -23,12 +24,22 @@ export async function getProducts(request: Request, response: Response) {
   response.status(200).send(result);
 }
 
-export async function updateProduct(request: Request, response: Response) {
-  const { idProduct } = response.locals;
-  const updateProductData = request.body;
-  await productsService.updateProduct(idProduct, updateProductData);
+export async function updateProduct(req: Request, res: Response) {
+  const { id } = req.params;
+  const { title, image, price, description, categoryId, unitOfMeasure, amount, quantityForUnity } = req.body;
 
-  response.sendStatus(200);
+  const updatedProduct = await productsService.updateProduct(id, {
+    title,
+    image,
+    price,
+    description,
+    categoryId,
+    unitOfMeasure,
+    amount,
+    quantityForUnity,
+  });
+
+  return res.status(200).send(updatedProduct);
 }
 
 export async function deleteProduct(request: Request, response: Response) {
